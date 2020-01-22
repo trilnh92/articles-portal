404ac1662fbba4a43e77532071bda0958537e439
Tri Le Nguyen Huu <trilnh92@gmail.com>
2020-01-22 15:47:59 +0700

    ~ change the destination link of Author Name on Footer

--- src/store/ArticleStore.js
+++ src/store/ArticleStore.js
@@ -1,6 +1,6 @@
-import {action, computed, observable, toJS} from "mobx";
-import ArticlesData from '../data/articles';
-import CategoriesData from '../data/categories';
+import { action, computed, observable, toJS } from 'mobx';
+import ArticlesData from '../data/articles.json';
+import CategoriesData from '../data/categories.json';
 
 class ArticleStore {
     @observable articles = ArticlesData;
@@ -11,32 +11,42 @@ class ArticleStore {
     @observable categoryId = ''; // default value of category id
 
     @computed get filteredArticles() {
-        return this.categoryId > 0 ? this.articles.filter(article => article.categoryId === this.categoryId) : this.articles;
+        return this.categoryId > 0
+            ? this.articles.filter(
+                  article => article.categoryId === this.categoryId
+              )
+            : this.articles;
     }
 
     @action
-    filteredFeaturedArticles = () =>  toJS(this.articles.filter(article => article.isFeaturedArticle));
+    filteredFeaturedArticles = () =>
+        toJS(this.articles.filter(article => article.isFeaturedArticle));
 
     @action
-    setCategoryId = categoryId => {this.categoryId = categoryId};
+    setCategoryId = categoryId => {
+        this.categoryId = categoryId;
+    };
 
     @action
-    getArticleById = articleId => toJS(this.articles.find(article => article.id === articleId));
+    getArticleById = articleId =>
+        toJS(this.articles.find(article => article.id === articleId));
 
     // TODO: split store
     @action
     createCategoriesData = () => {
-        const defaultValue = {value: 0, label: 'All Categories'};
+        const defaultValue = { value: 0, label: 'All Categories' };
         if (this.categories[0].label !== defaultValue.label) {
-            this.categories.unshift(defaultValue)
+            this.categories.unshift(defaultValue);
         }
         return toJS(this.categories);
     };
 
     // TODO: split store
     @action
-    displayCategoryName = (categoryId) => {
-        const match = this.categories.find(category => category.value === categoryId);
+    displayCategoryName = categoryId => {
+        const match = this.categories.find(
+            category => category.value === categoryId
+        );
         return match ? toJS(match.label) : 'Not Found';
     };
 }
